6596c648ff35698d3c4a3f838262afddc56075a4
alexfabianreforzar <alejandromacchione@gmail.com>
2020-12-29 13:38:11 -0300

    Se modifica Productos y Galeria

--- js/app.js
+++ js/app.js
@@ -8,20 +8,33 @@ function getFilename(path) {
     return path.split('/').pop().split('.')[0];
 }
 
-function getCarouselIndicator(src, index) {
-    return '<li id="' + getFilename(src) + 'Indicator" class="carousel-indicator" data-target="#carouselGallery" data-slide-to="' + index + '"></li>';
+function getCarouselIndicator(src) {
+    return '<li id="' + getFilename(src) + 'Indicator" class="carousel-indicator" data-target="#carouselGallery" data-slide-to="' + $('#carouselGallery .carousel-indicator').length + '"></li>';
 }
 
-function getCarouselItem(src, alt) {
-    return '<div id="' + getFilename(src) + 'Item" class="carousel-item"><img class="d-block w-100" src="' + src + '" alt="' + alt + '"/></div>';
+function getCarouselItem(src) {
+    return '<div id="' + getFilename(src) + 'Item" class="carousel-item justify-content-center">CAROUSEL_ITEM</div>';
+}
+
+function getCarouselImg(src, alt) {
+    return getCarouselItem(src).replace('CAROUSEL_ITEM', '<img class="d-block w-100" src="' + src + '" alt="' + alt + '"/>');
+}
+
+function getCarouselVideo(src) {
+    return getCarouselItem(src).replace('CAROUSEL_ITEM', '<video controls><source src="' + src + '" type="video/mp4">Video no soportado.</video>');
 }
 
 function openGallery(elem) {
     $('#carouselGallery .carousel-indicator').removeClass('active');
     $('#carouselGallery .carousel-item').removeClass('active');
+
+    if (!elem) {
+        elem = $('#gallery img')[0];
+    }
+
     $('#' + getFilename($(elem).attr('src')) + 'Indicator').addClass('active');
     $('#' + getFilename($(elem).attr('src')) + 'Item').addClass('active');
-    
+
     $('#galleryModal').modal('show');
 }
 
@@ -30,9 +43,32 @@ function initGallery() {
         openGallery($(this).children());
     });
 
-    $('#gallery img').each(function(index) {
-        $('#carouselGallery .carousel-indicators').append(getCarouselIndicator($(this).attr('src'), index));
-        $('#carouselGallery .carousel-inner').append(getCarouselItem($(this).attr('src'), $(this).attr('alt')));
+    $('#gallery img').each(function() {
+        $('#carouselGallery .carousel-indicators').append(getCarouselIndicator($(this).attr('src')));
+        $('#carouselGallery .carousel-inner').append(getCarouselImg($(this).attr('src'), $(this).attr('alt')));
+    });
+
+    let videos = [
+        'videos/video1.mp4',
+        'videos/video2.mp4',
+        'videos/video3.mp4'
+    ];
+
+    videos.forEach(function(src) {
+        $('#carouselGallery .carousel-indicators').append(getCarouselIndicator(src));
+        $('#carouselGallery .carousel-inner').append(getCarouselVideo(src));
+    });
+
+    $('#carouselGallery').on('slide.bs.carousel', function () {
+        $('#carouselGallery video').each(function() {
+            $(this)[0].pause();
+        });
+
+        $('#carouselGallery .carousel-indicators').fadeIn();
+    });
+
+    $('#carouselGallery video').on('playing', function() {
+        $('#carouselGallery .carousel-indicators').fadeOut();
     });
 }
 
@@ -44,12 +80,12 @@ $(document).ready(function() {
           $('#backToTopBtn').fadeOut();
        }
     });
-    
+
     $('body').scrollspy({
         target: '#navbarMenu',
         offset: 300
     });
-    
+
     $('.nav-link').click(function(e) {
         e.preventDefault();
         $('html, body').animate({
